c171186467f5298b87f7f9704c1b2e291d27ffeb
keisak1 <41781@ufp.edu.pt>
2025-10-04 19:05:26 +0100

    Initial commit

---
+++ src/main/java/com/nuno/expensewiseapi/ExpensewiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuno.expensewiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensewiseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensewiseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuno/expensewiseapi/ExpensewiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuno.expensewiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensewiseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
